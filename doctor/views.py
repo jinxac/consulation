530cad8156f620e04fb341972e38d64cf71d121f
Harkirat Saluja <saluja.harkirat@gmail.com>
2020-03-03 11:33:33 +0530

    Adding permission class to doctor

--- doctor/views.py
+++ doctor/views.py
@@ -2,12 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Doctor
 from .serializer import DoctorSerializer
 
 
 class DoctorList(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         doctors = Doctor.objects.all()
         serializer = DoctorSerializer(doctors, many=True)
