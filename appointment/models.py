4b1be8efad3191c3ddf93ad11ad529d5e4da319c
Harkirat Saluja <saluja.harkirat@gmail.com>
2020-03-03 11:15:14 +0530

    cleaning up models a bit

--- appointment/models.py
+++ appointment/models.py
@@ -3,22 +3,50 @@ from django.db import models
 from commons.models.models import LogicalDeleteModel
 
 
-from office.models import Office
+from doctor.models import Doctor
 from client.models import Client
 
 
 class AppointmentStatus(models.IntegerChoices):
     Created = 0
-    Active = 1
+    Completed = 1
     Cancelled = 2
 
 
+# TODO: Move to utils
+def get_upload_url(full_key):
+    print(full_key)
+    # TODO: Add logic to upload the doc
+
+
 class Appointment(LogicalDeleteModel):
-    office = models.ForeignKey(Office, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    probable_start_time = models.DateTimeField()
-    actual_start_time = models.DateTimeField()
-    actual_end_time = models.DateTimeField()
-    status = models.IntegerField(choices=AppointmentStatus.choices)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     appointment_date = models.DateTimeField()
+    status = models.IntegerField(choices=AppointmentStatus.choices)
+
+
+class Record(LogicalDeleteModel):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    doc_id = models.CharField(max_length=255)
+
+    @property
+    def record_url(self):
+        full_key = "{}/{}".format(
+            self.client.id, self.doc_id
+        )
+        return get_upload_url(full_key)
+
+
+class Feedback(LogicalDeleteModel):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    is_anonymous = models.BooleanField()
+    rating = models.IntegerField()
+    description = models.CharField(max_length=255)
+
 
